50e7fa0edbcd9dab0dd8f7f8b4c27257c68b92f1
Shahadat171 <shahadathosan999@gmail.com>
2023-07-23 20:54:26 +0600

    Home page api completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,14 @@ async function run() {
       const result = await cursor.limit(3).toArray();
       res.send(result);
     });
+
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await College.collection("allcolleges").findOne(query);
+      res.send(result);
+    });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
